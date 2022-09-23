8a7e9859aad1135d538440f1a8704f901d03d345
~ fu (￣▽￣@) <45820630+inokoe@users.noreply.github.com>
2022-09-23 20:49:57 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ Vue.use(VueLazyload, {
   error: errorimage,
   loading: loadimage,
   observer: true,
-  throttleWait: 0,
+  throttleWait: 300,
   observerOptions: {
     rootMargin: "0px",
     threshold: 0.1,
